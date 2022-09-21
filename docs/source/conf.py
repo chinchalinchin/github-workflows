d595af0aa232160c41dd67f27af4ed5308d761b3
Grant Moore <625518@bah.com>
2022-09-21 12:04:36 -0400

    update docs styles

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,21 +15,17 @@ release = 'your-release'
 # NOTE: the rest of the configuration should not be changed, unless you want to customize
 #        the appearance of your documentation page.
 
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'myst_parser', 
-    'sphinx.ext.githubpages'
-]
+extensions = ['myst_parser', 'sphinx.ext.githubpages']
 source_suffix = {
     '.rst': 'restructuredtext',
     '.md': 'markdown',
 }
 # Add any paths that contain templates here, relative to this directory.
-templates_path = [
-    '_templates'
-]
+templates_path = ['_templates']
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -39,18 +35,45 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = 'alabaster'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [
-    '_static'
-]
+html_static_path = ['_static']
 
 html_use_index = True
 
 master_doc = 'index'
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+
+html_theme = 'sphinx_material'
+
+# Material theme options (see theme.conf for more information)
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'Automation Library',
+
+    # Set you GA account ID to enable tracking
+    'google_analytics_account': 'UA-XXXXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://pages.github.boozallencsn.com/AutomationLibrary/aws-synthetics-heartbeat-canary/',
+
+    # Set the color and the accent color
+    'color_primary': 'blue',
+    'color_accent': 'light-blue',
+
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.boozallencsn.com/AutomationLibrary/aws-synthetics-heartbeat-canary',
+    'repo_name': 'aws-synthetics-heartbeat-canary',
+
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': -1,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+}
